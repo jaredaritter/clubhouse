747841762646942e5ae7342121bb931a464999e3
jared <38332939+jaredaritter@users.noreply.github.com>
2021-03-18 21:19:36 -0400

    Add passport local strategy.

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
+// ---------------------------------------
+// ** MODULES **
+// ---------------------------------------
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+// const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
 
-// IMPORT ROUTES
+// ---------------------------------------
+// ** IMPORT ROUTES **
+// ---------------------------------------
 const indexRouter = require('./routes/index.js');
 
-// MONGO CONNECTION
+// ---------------------------------------
+// ** MONGO CONNECTION **
+// ---------------------------------------
 const mongoDB = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.jrymf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 mongoose.connect(mongoDB, {
   useUnifiedTopology: true,
@@ -18,12 +26,46 @@ mongoose.connect(mongoDB, {
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Mongo connection error'));
 
-// APP SETUP
+// ---------------------------------------
+// ** APP SETUP **
+// ---------------------------------------
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// MIDDLEWARE
+// ---------------------------------------
+// ** PASSPORT SETUP **
+// ---------------------------------------
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+// ---------------------------------------
+// ** MIDDLEWARE **
+// ---------------------------------------
 app.use(
   session({
     secret: 'Penny is the best',
@@ -35,10 +77,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-// ROUTES
+// ---------------------------------------
+// ** ROUTES **
+// ---------------------------------------
 app.use('/', indexRouter);
 
-// LISTENER
+// ---------------------------------------
+// ** LISTENER **
+// ---------------------------------------
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
