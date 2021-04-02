eaea1fcf87320e66c7834d9fa75288499b08a030
jared <38332939+jaredaritter@users.noreply.github.com>
2021-04-02 12:24:28 -0400

    Change key name in Message model.

--- models/Message.js
+++ models/Message.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const MessageSchema = new Schema({
   title: { type: String, required: true },
   date: { type: Date, default: Date.now, required: true },
-  text: { type: String, required: true },
+  message: { type: String, required: true },
 });
 
 module.exports = mongoose.model('Message', MessageSchema);
